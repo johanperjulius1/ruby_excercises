c3f611428553ceca4d4707994ae84449286ed276
Johan Berglund <johan.per.julius.berglund@gmail.com>
2020-08-12 16:56:49 +0200

    I added all the previously mentioned variable, hashes and data to my ruby file

---
+++ my_group.rb
@@ -0,0 +1,5 @@
+my_group = ["person_1", "person_2", "person_3"]
+
+person_1 = {name:"Johan", gender: "male", age:33}
+person_2 = {name: "Helena", gender: "female", age:26}
+person_3 = {name: "Eric", gender: "male", age:30}
